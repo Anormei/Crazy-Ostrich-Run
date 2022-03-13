867667f80b2187f898deb446644cf84079708497
Steven <Steven.Warlow@mkodo.com>
2022-03-13 04:04:26 +0000

    Scene changes and empty custom menu for game object edits

---
+++ Assets/UnityEditor/GameObjectCustomMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GameObjectCustomMenu : MonoBehaviour
+{
+    [MenuItem("Edit GameObject/Center game objects within model")]
+    static void centerWithinModel()
+    {
+
+    }
+}
