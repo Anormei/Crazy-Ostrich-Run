d9b3f77316117245c5960641c5cbea1ed97fe2c2
Steven <Steven.Warlow@mkodo.com>
2022-04-02 22:09:04 +0100

    Add crate -> player interaction on collision

---
+++ Assets/CratePlayerInteraction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CratePlayerInteraction : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float hitForce;
+    [SerializeField]
+    private float hitSpinForce;
+
+    
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        GetComponent<BoxCollider2D>().enabled = true;
+        GetComponent<ObjectScroller>().enabled = true;
+        transform.localRotation = Quaternion.identity;
+    }
+
+    void OnDisable()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<ObjectScroller>().enabled = false;
+            rb.AddForce(new Vector3(hitForce, hitForce, 0));
+            rb.AddTorque(-hitSpinForce);
+        }
+    }
+}
